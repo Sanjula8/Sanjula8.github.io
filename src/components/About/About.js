9950e551ca3530c43911c8a6c5ee47f97d770257
Sanjula <sanjula8@gmail.com>
2020-04-27 15:41:42 -0500

    minor changes to allow readability

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -43,8 +43,8 @@ function About() {
 									</a>
 								</p>
 							</div>
-							<div className="skills">Skills</div>
-							<div>
+							<div className="skills-title">Skills</div>
+							<div className="skills">
 								Git | JavaScript | Node.js | React.js | CSS |
 								HTML5 ES6 | Express.js | AJAX MERN Stack |
 								MongoDB | Mongoose MySQL | Sequelize
